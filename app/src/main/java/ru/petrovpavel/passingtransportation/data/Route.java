24f71dd658aaf0f38989814debe334ace6c6fd94
Prioravod <Prioravod@github.com>
2022-01-30 04:15:35 +0400

    added Route toString template

--- app/src/main/java/ru/petrovpavel/passingtransportation/data/Route.java
+++ app/src/main/java/ru/petrovpavel/passingtransportation/data/Route.java
@@ -1,9 +1,14 @@
 package ru.petrovpavel.passingtransportation.data;
 
+import java.text.MessageFormat;
+import java.util.Optional;
+
 import lombok.Data;
 
 @Data
 public class Route {
+
+    private static final String DISPLAY_TEMPLATE = "{0} -> {1} {3} кг {4, number, double} р";
 
     private MapPoint origin;
 
@@ -12,4 +17,15 @@ public class Route {
     private Double weight;
 
     private Double payment;
+
+    @Override
+    public String toString() {
+        String from = Optional.ofNullable(origin)
+                .map(MapPoint::getAlias)
+                .orElse("");
+        String to = Optional.ofNullable(destination)
+                .map(MapPoint::getAlias)
+                .orElse("");
+        return MessageFormat.format(DISPLAY_TEMPLATE, from, to, weight);
+    }
 }
