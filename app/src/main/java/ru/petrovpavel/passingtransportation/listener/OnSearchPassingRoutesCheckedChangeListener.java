adb457e7fbe46bef2b3f7ad914e5546a358af1ce
Prioravod <Prioravod@github.com>
2022-02-05 21:47:55 +0400

    Updated VehicleHolder setters using for init delay

--- app/src/main/java/ru/petrovpavel/passingtransportation/listener/OnSearchPassingRoutesCheckedChangeListener.java
+++ app/src/main/java/ru/petrovpavel/passingtransportation/listener/OnSearchPassingRoutesCheckedChangeListener.java
@@ -3,6 +3,8 @@ package ru.petrovpavel.passingtransportation.listener;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
+import androidx.appcompat.widget.AppCompatEditText;
+
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Optional;
@@ -15,10 +17,21 @@ import ru.petrovpavel.passingtransportation.data.memory.VehicleHolder;
 public class OnSearchPassingRoutesCheckedChangeListener implements CompoundButton.OnCheckedChangeListener {
 
     private final TextInputEditText capacityValue;
+    private final TextInputEditText delayValue;
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean isEnabled) {
         VehicleHolder vehicleHolder = VehicleHolder.getInstance();
         vehicleHolder.setEnabled(isEnabled);
+        vehicleHolder.setAvailableCapacity(getNumericValue(capacityValue));
+        vehicleHolder.setDelay(getNumericValue(delayValue));
+    }
+
+    private Integer getNumericValue(TextInputEditText delayValue) {
+        return Optional.ofNullable(delayValue)
+                .map(AppCompatEditText::getText)
+                .map(Object::toString)
+                .map(Integer::parseInt)
+                .orElse(null);
     }
 }
