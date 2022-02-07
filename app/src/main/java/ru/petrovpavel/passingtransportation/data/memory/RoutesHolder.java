26b4f4b3a8e791ea610e26a1b99a467537e40de2
Prioravod <Prioravod@github.com>
2022-02-08 01:27:33 +0400

    updated RouteHolder

--- app/src/main/java/ru/petrovpavel/passingtransportation/data/memory/RoutesHolder.java
+++ app/src/main/java/ru/petrovpavel/passingtransportation/data/memory/RoutesHolder.java
@@ -2,33 +2,56 @@ package ru.petrovpavel.passingtransportation.data.memory;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ru.petrovpavel.passingtransportation.data.Route;
+import ru.petrovpavel.passingtransportation.data.RouteStatus;
+import ru.petrovpavel.passingtransportation.data.RouteWrapper;
 
 public class RoutesHolder {
 
-    private final List<Route> availableRoutes = new LinkedList<>();
+    private final List<RouteWrapper> availableRoutes = new LinkedList<>();
 
-    public List<Route> getAvailableRoutes() {
-        return availableRoutes;
+    public List<RouteWrapper> getAvailableRoutes() {
+        return availableRoutes.stream()
+                .filter(routeWrapper -> RouteStatus.NEW.equals(routeWrapper.getStatus()))
+                .collect(Collectors.toList());
+    }
+
+    public RouteWrapper getCurrentRoute() {
+        return availableRoutes.stream()
+                .filter(routeWrapper -> RouteStatus.CONFIRMED.equals(routeWrapper.getStatus()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void finishTrip() {
+        availableRoutes.stream()
+                .filter(routeWrapper -> RouteStatus.CONFIRMED.equals(routeWrapper.getStatus()))
+                .findFirst()
+                .ifPresent(routeWrapper -> routeWrapper.setStatus(RouteStatus.PASSED));
     }
 
     public void addAvailableRoute(Route newRoute) {
-        availableRoutes.add(newRoute);
+        availableRoutes.add(new RouteWrapper(newRoute, RouteStatus.NEW));
     }
 
-    public void addAvailableRouteIfAbsent(Route newRoute) {
-        if (!availableRoutes.contains(newRoute)) {
-            availableRoutes.add(newRoute);
-        }
-    }
-
-    public void removeAvailableRoute(Route newRoute) {
-        availableRoutes.remove(newRoute);
+    public void removeAvailableRoute(Route deletedRoute) {
+        updateStatus(deletedRoute, RouteStatus.DELETED);
     }
 
     public void flushAvailableRoutes() {
         availableRoutes.clear();
+    }
+
+    public void updateStatus(Route route, RouteStatus status) {
+        availableRoutes.stream()
+                .filter(routeWrapper ->
+                        routeWrapper.getRoute() != null
+                        && route != null
+                        && route.equals(routeWrapper.getRoute()))
+                .findFirst()
+                .ifPresent(routeWrapper -> routeWrapper.setStatus(status));
     }
 
     private static final RoutesHolder holder = new RoutesHolder();
